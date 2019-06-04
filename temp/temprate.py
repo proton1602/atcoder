94774411c729b4373abd5034ead770f81b230150
proton <kawashima-m@aidemy.net>
2019-06-05 01:16:33 +0900

    temp追加

--- temp/temprate.py
+++ temp/temprate.py
@@ -1,3 +1,5 @@
+import sys
+input = sys.stdin.readline
 ri = lambda: int(input())
 rl = lambda: list(map(int,input().split()))
 rr = lambda N: [ri() for _ in range(N)]
@@ -5,4 +7,5 @@ YN = lambda b: print('YES') if b else print('NO')
 yn = lambda b: print('Yes') if b else print('No')
 OE = lambda x: print('Odd') if x%2 else print('Even')
 INF = 10**18
+MOD=10**9+7
 
