3418727d75b719a5f2dc8051aeee1e4e82ece28e
proton <kawashima-m@aidemy.net>
2019-06-19 21:24:18 +0900

    abc125

--- abc125/c.py
+++ abc125/c.py
@@ -11,3 +11,23 @@ OE = lambda x: print('Odd') if x%2 else print('Even')
 INF = 10**18
 MOD = 10**9+7
 
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a%b
+    return a
+
+N=ri()
+A=list(rl())
+L=[0]*(N+2)
+R=[0]*(N+2)
+L[0] = A[0]
+R[N-1] = A[N-1]
+for i in range(1,N):
+    L[i] = gcd(L[i-1],A[i])
+for i in range(N-2,-1,-1):
+    R[i] = gcd(R[i+1],A[i])
+
+ans = 0
+for i in range(N):
+    ans = max(ans,gcd(L[i-1],R[i+1]))
+print(ans)
